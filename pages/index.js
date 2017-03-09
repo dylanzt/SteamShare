8bc9cac928867c4a7d5f47b901340333be3a14a1
Dylan <dylan.treisman@mi9.com.au>
2017-03-09 20:02:40 +1100

    Add API call logic

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import 'isomorphic-fetch';
+import getUserInfo from '../lib/userInfo';
+import { getFriendInfo, getUserGames, getSharedGames, getGameInfo } from '../lib/userInfo';
 
 export default class extends React.Component {
-  static getInitialProps () {
-    return fetch('http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=FDF267D8BAEE833D4569D2433FB4AD4F&steamid=76561197960435530&relationship=friend')
-    .then((res) => {
-        return res.json();
-    }).then((data) => {
-        return { friends: data.friendslist };
-    });
+  static async getInitialProps () {
+    //   return getUserInfo('76561197964790875').then((players) => ({ players }));
+    //   return getFriendInfo('76561197964790875').then((players) => ({ players }));
+    // return getUserGames('76561197964790875').then((players) => ({ players }));
+      return getSharedGames(['76561197964790875','76561198105819920']).then((players) => ({ players }));
+    //   return getGameInfo('292030').then((players) => ({ players }));
   }
 
   render() {
     return (
         <div>
-            {JSON.stringify(this.props.friends)}
+            {JSON.stringify(this.props.players) }
         </div>
     );
   }
