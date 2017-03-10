c9b4cf96fc8d9db31e6ed07b3a52efc953da8341
Varun Balakrishna <varun.balakrishna@mi9.com.au>
2017-03-10 14:55:02 +1100

    finish the thing

--- components/CommonGames.js
+++ components/CommonGames.js
@@ -6,15 +6,31 @@ const mapStateToProps = ({ commonGames }) => {
 
 const CommonGames = ({ commonGames }) => {
   return (
-    <div>
+    <div className="common-games">
       {commonGames.length ? commonGames.map((game) => {
         return (
-          <li>
+          <li className="">
             <img src={game.gameImage} />
             <h1>{game.gameName}</h1>
           </li>
         );
       }) : <h1>No games in common!</h1>}
+      <style jsx>{`
+        .common-games {
+          display: flex;
+          flex-direction: column;
+          font-family: Roboto Arial sans-serif;
+        }
+
+        img {
+          margin-right: 16px;
+        }
+
+        li {
+          display: flex;
+
+        }
+      `}</style>
     </div>
   )
 };
