1c911fa34775cc1723f47b6db6bd6817e6bb7252
Varun Balakrishna <varun.balakrishna@mi9.com.au>
2017-03-10 14:48:28 +1100

    Basic error message

--- components/CommonGames.js
+++ components/CommonGames.js
@@ -7,14 +7,14 @@ const mapStateToProps = ({ commonGames }) => {
 const CommonGames = ({ commonGames }) => {
   return (
     <div>
-      {commonGames.map((game) => {
+      {commonGames.length ? commonGames.map((game) => {
         return (
           <li>
             <img src={game.gameImage} />
             <h1>{game.gameName}</h1>
           </li>
         );
-      })}
+      }) : <h1>No games in common!</h1>}
     </div>
   )
 };
